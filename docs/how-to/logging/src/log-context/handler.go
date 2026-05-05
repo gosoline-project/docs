892282b05620a247d2fe5650d4bfe883b949495d
Jan Kamieth <jan@justtrack.io>
2026-05-05 22:05:47 +0200

    docs: update outdated code to new gosoline APIs and config format
    - migrated all config files to new app identity format (no tags)
    - rewrote log-context example from old httpserver/crud to new With+Bind pattern
    - fixed invalid YAML in health-check config (removed highlight comments)

--- docs/how-to/logging/src/log-context/handler.go
+++ docs/how-to/logging/src/log-context/handler.go
@@ -3,80 +3,53 @@ package main
 import (
 	"context"
 	"fmt"
+	"strconv"
+	"sync"
 	"time"
 
+	"github.com/gosoline-project/httpserver"
 	"github.com/justtrackio/gosoline/pkg/cfg"
-	"github.com/justtrackio/gosoline/pkg/db-repo"
-	"github.com/justtrackio/gosoline/pkg/httpserver/crud"
 	"github.com/justtrackio/gosoline/pkg/log"
-	"github.com/justtrackio/gosoline/pkg/mdl"
 )
 
-var settings = db_repo.Settings{
-	Metadata: db_repo.Metadata{
-		ModelId: mdl.ModelId{
-			Name: "todo",
-		},
-		TableName:  "todos",
-		PrimaryKey: "todos.id",
-	},
+type CreateTodoInput struct {
+	Text    string    `json:"text"`
+	DueDate time.Time `json:"dueDate"`
+}
+
+type UpdateTodoInput struct {
+	Id   int    `uri:"id"`
+	Text string `json:"text"`
 }
 
 type Todo struct {
-	db_repo.Model
-	Text    string
-	DueDate time.Time
-}
-
-type CreateInput struct {
-	Text    string    `form:"text"`
-	DueDate time.Time `form:"dueDate"`
-}
-
-type UpdateInput struct {
-	Text string `form:"text"`
+	Id      int       `json:"id"`
+	Text    string    `json:"text"`
+	DueDate time.Time `json:"dueDate"`
 }
 
 // snippet-start: crud handler v0
-type TodoCrudHandlerV0 struct {
+type TodoHandler struct {
 	// highlight-next-line
 	logger log.Logger
-	repo   db_repo.Repository
+	mu     sync.Mutex
+	nextId int
+	todos  map[int]*Todo
 }
 
 // snippet-end: crud handler v0
 
 // snippet-start: new todo crud handler
-func NewTodoCrudHandler(ctx context.Context, config cfg.Config, logger log.Logger) (*TodoCrudHandlerV0, error) {
-	var err error
-	var repo db_repo.Repository
-
-	if repo, err = db_repo.New(ctx, config, logger, settings); err != nil {
-		return nil, fmt.Errorf("can not create db_repo.Repositorys: %w", err)
-	}
-
-	handler := &TodoCrudHandlerV0{
+func NewTodoHandler(ctx context.Context, config cfg.Config, logger log.Logger) (*TodoHandler, error) {
+	return &TodoHandler{
 		// highlight-next-line
 		logger: logger,
-		repo:   repo,
-	}
-
-	return handler, nil
+		nextId: 1,
+		todos:  map[int]*Todo{},
+	}, nil
 }
 
 // snippet-end: new todo crud handler
-
-func (h TodoCrudHandlerV0) GetRepository() crud.Repository {
-	return h.repo
-}
-
-func (h TodoCrudHandlerV0) GetModel() db_repo.ModelBased {
-	return &Todo{}
-}
-
-func (h TodoCrudHandlerV0) GetCreateInput() any {
-	return &CreateInput{}
-}
 
 // snippet-start: truncate
 func truncate(ctx context.Context, text string) string {
@@ -96,64 +69,53 @@ func truncate(ctx context.Context, text string) string {
 
 // snippet-end: truncate
 
-// snippet-start: transform create
-func (h TodoCrudHandlerV0) TransformCreate(ctx context.Context, input any, model db_repo.ModelBased) error {
-	in := input.(*CreateInput)
-	m := model.(*Todo)
+func (h *TodoHandler) CreateTodo(ctx context.Context, input *CreateTodoInput) (httpserver.Response, error) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
 
 	// highlight-start
 	localctx := log.InitContext(ctx)
-	m.Text = truncate(localctx, in.Text)
+	text := truncate(localctx, input.Text)
 	// highlight-end
-	m.DueDate = in.DueDate
 
-	return nil
+	todo := &Todo{
+		Id:      h.nextId,
+		Text:    text,
+		DueDate: input.DueDate,
+	}
+	h.todos[todo.Id] = todo
+	h.nextId++
+
+	// highlight-next-line
+	h.logger.Info(localctx, "creating new task due at %v", todo.DueDate)
+
+	return httpserver.NewJsonResponse(todo), nil
 }
 
-// snippet-end: transform create
+func (h *TodoHandler) UpdateTodo(ctx context.Context, input *UpdateTodoInput) (httpserver.Response, error) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
 
-func (h TodoCrudHandlerV0) GetUpdateInput() any {
-	return &UpdateInput{}
-}
-
-// snippet-start: transform update
-func (h TodoCrudHandlerV0) TransformUpdate(ctx context.Context, input any, model db_repo.ModelBased) error {
-	in := input.(*UpdateInput)
-	m := model.(*Todo)
+	todo, ok := h.todos[input.Id]
+	if !ok {
+		return httpserver.NewStatusResponse(404), fmt.Errorf("todo %d not found", input.Id)
+	}
 
 	// highlight-start
 	localctx := log.InitContext(ctx)
-	m.Text = truncate(localctx, in.Text)
+	todo.Text = truncate(localctx, input.Text)
 	// highlight-end
 
-	return nil
+	return httpserver.NewJsonResponse(todo), nil
 }
 
-// snippet-end: transform update
-
-func (h TodoCrudHandlerV0) TransformOutput(ctx context.Context, model db_repo.ModelBased, apiView string) (any, error) {
-	return model, nil
-}
-
-func (h TodoCrudHandlerV0) List(ctx context.Context, qb *db_repo.QueryBuilder, apiView string) (any, error) {
-	var err error
-
-	// Instatiate a list of Todo objects, called result.
-	result := make([]*Todo, 0)
-
-	// Query the database using a Context and a QueryBuilder. If it finds the results, it sets them on result. Otherwise, it returns an error.
-	if err = h.repo.Query(ctx, qb, &result); err != nil {
-		return nil, fmt.Errorf("can not query todo items: %w", err)
+// snippet-start: parse id helper
+func parseId(id string) (int, error) {
+	n, err := strconv.Atoi(id)
+	if err != nil {
+		return 0, fmt.Errorf("invalid id: %s", id)
 	}
-
-	// Transform each result with TransformOutput().
-	out := make([]any, len(result))
-	for i, res := range result {
-		if out[i], err = h.TransformOutput(ctx, res, apiView); err != nil {
-			return nil, err
-		}
-	}
-
-	// Return the transformed results.
-	return out, nil
+	return n, nil
 }
+
+// snippet-end: parse id helper
--- docs/how-to/logging/src/log-context/main.go
+++ docs/how-to/logging/src/log-context/main.go
@@ -5,10 +5,9 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/gosoline-project/httpserver"
 	"github.com/justtrackio/gosoline/pkg/application"
 	"github.com/justtrackio/gosoline/pkg/cfg"
-	"github.com/justtrackio/gosoline/pkg/httpserver"
-	"github.com/justtrackio/gosoline/pkg/httpserver/crud"
 	"github.com/justtrackio/gosoline/pkg/log"
 )
 
@@ -16,28 +15,14 @@ import (
 
 // snippet-start: main
 func main() {
-	definer := func(ctx context.Context, config cfg.Config, logger log.Logger) (*httpserver.Definitions, error) {
-		// Instantiated a new Definitions object.
-		def := &httpserver.Definitions{}
+	httpserver.RunDefaultServer(func(ctx context.Context, config cfg.Config, logger log.Logger, router *httpserver.Router) error {
+		router.HandleWith(httpserver.With(NewTodoHandler, func(r *httpserver.Router, h *TodoHandler) {
+			r.POST("/todos", httpserver.Bind(h.CreateTodo))
+			r.PUT("/todos/:id", httpserver.Bind(h.UpdateTodo))
+		}))
 
-		var err error
-		var handler crud.Handler
-
-		// Created a new CRUD handler.
-		if handler, err = NewTodoCrudHandler(ctx, config, logger); err != nil {
-			return nil, fmt.Errorf("can not create trip handler: %w", err)
-		}
-
-		// Add CRUD handlers to your definitions. This is a convenience method for adding handlers for Create, Read, Update, Delete, and List.
-		if err := crud.AddCrudHandlers(config, logger, def, 0, "todo", handler); err != nil {
-			return nil, fmt.Errorf("can not add crud handlers: %w", err)
-		}
-
-		return def, nil
-	}
-
-	// Run your server with the definitions.
-	application.RunHttpDefaultServer(definer,
+		return nil
+	},
 		application.WithConfigFile("config.dist.yml", "yml"),
 	)
 }
